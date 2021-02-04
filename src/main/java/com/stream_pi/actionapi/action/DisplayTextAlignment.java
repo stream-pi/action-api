3759bd6509177a8f0c5d783c1bf06349b387da1a
rnayabed <debayansutradhar3@gmail.com>
2021-02-04 22:52:16 +0530

    Added UI Name component for DisplayTextAlignment

--- src/main/java/com/stream_pi/actionapi/action/DisplayTextAlignment.java
+++ src/main/java/com/stream_pi/actionapi/action/DisplayTextAlignment.java
@@ -1,5 +1,15 @@
 package com.stream_pi.actionapi.action;
 
 public enum DisplayTextAlignment {
-    TOP, CENTER, BOTTOM
+    TOP("Top"), CENTER("Center"), BOTTOM("Bottom");
+
+    private final String UIName;
+    DisplayTextAlignment(String UIName)
+    {
+        this.UIName = UIName;
+    }
+
+    public String getUIName() {
+        return UIName;
+    }
 }
