1d69b26bd135c8bc9770061d32da0ef36e4129dd
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-06-03 08:52:55 +0530

    Add TemporaryListProperty

---
+++ src/main/java/com/stream_pi/action_api/actionproperty/property/TemporaryListProperty.java
@@ -0,0 +1,34 @@
+package com.stream_pi.action_api.actionproperty.property;
+
+import com.stream_pi.util.exception.MinorException;
+
+import java.util.List;
+
+public class TemporaryListProperty extends Property
+{
+    public TemporaryListProperty(String name)
+    {
+        super(name, Type.STRING);
+    }
+
+    public void setList(List<String> list)
+    {
+        this.list = list;
+        this.rawValue = list.get(0);
+    }
+
+    public void setValue(String value)
+    {
+        setRawValue(value);
+    }
+
+    public String getValue()
+    {
+        return getRawValue();
+    }
+
+    public List<String> getList()
+    {
+        return list;
+    }
+}
