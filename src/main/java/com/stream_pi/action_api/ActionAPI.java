33cd21de9767b623c9bb94bc347d89091b266cde
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-06-17 13:16:20 +0530

    cleanup

--- src/main/java/com/stream_pi/action_api/ActionAPI.java
+++ src/main/java/com/stream_pi/action_api/ActionAPI.java
@@ -3,6 +3,7 @@ package com.stream_pi.action_api;
 
 import com.stream_pi.util.version.Version;
 
-public class ActionAPI {
+public class ActionAPI
+{
     public static final Version API_VERSION = new Version(1,0,0);
 }
